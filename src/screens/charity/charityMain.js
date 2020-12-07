5e15216408ddb2b8ccb4bbce84ba15ad3db19f65
ZzRanger <calebmarklim@gmail.com>
2020-12-07 11:44:21 -0600

    reworked charity screens

--- src/screens/charity/charityMain.js
+++ src/screens/charity/charityMain.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Button, Icon } from 'react-native';
 
 import InventoryCard from '../../components/charity/inventoryCard'
+import InventoryStatusCard from '../../components/charity/inventoryStatusCard'
 
 export default function CharityMain(props) {
     const { route, navigation } = props;
     const data = route.params;
-    console.log("Charity Main Data", data);
+    // console.log("Charity Main Data", data);
     return(
         <View>
             <InventoryCard data = {data}/>    
+            <InventoryStatusCard />
         </View>
     )
 }

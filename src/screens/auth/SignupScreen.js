65a5a2932c21e0aaf119999d1a5509859f28edf4
Gerald <gprmarquez@gmail.com>
2020-12-06 18:20:54 -0800

    added firebase.js service

--- src/screens/auth/SignupScreen.js
+++ src/screens/auth/SignupScreen.js
@@ -7,6 +7,7 @@ import { StackNavigator } from 'react-navigation';
 import { Input } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native'
 import AuthContext from "../../../Context"
+import { db } from '../../services/firebase'
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -16,20 +17,6 @@ const styles = StyleSheet.create({
     },
 });
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAfeiT7Qp96l-iBUg_4I5ymj4yEc4Cx-k4",
-    authDomain: "giv4gud-dcd30.firebaseapp.com",
-    databaseURL: "https://giv4gud-dcd30.firebaseio.com",
-    projectId: "giv4gud-dcd30",
-    storageBucket: "giv4gud-dcd30.appspot.com",
-    messagingSenderId: "884472859704",
-    appId: "1:884472859704:web:e4189e20a69609b6216acd"
-};
-
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
 export default function SignupScreen() {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
@@ -37,6 +24,20 @@ export default function SignupScreen() {
     const [confirmPassword, setConfirmPassword] = useState('')
     const navigation = useNavigation()
     const { signUp } = React.useContext(AuthContext);
+    const data = {
+        stringExample: 'Hello, World!',
+        booleanExample: true,
+        numberExample: 3.14159265,
+        arrayExample: [5, true, 'hello'],
+        nullExample: null,
+        objectExample: {
+            a: 5,
+            b: true
+        }
+    };
+
+    db.collection('data').doc('two').set(data);
+
     const onRegisterPress = () => {
         if (password !== confirmPassword) {
             alert("Passwords don't match.")
---
+++ src/services/firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAfeiT7Qp96l-iBUg_4I5ymj4yEc4Cx-k4",
+    authDomain: "giv4gud-dcd30.firebaseapp.com",
+    databaseURL: "https://giv4gud-dcd30.firebaseio.com",
+    projectId: "giv4gud-dcd30",
+    storageBucket: "giv4gud-dcd30.appspot.com",
+    messagingSenderId: "884472859704",
+    appId: "1:884472859704:web:e4189e20a69609b6216acd"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+
+export {
+    db
+}

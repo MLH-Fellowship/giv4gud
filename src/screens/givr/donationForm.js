6e0188cf5cb81875a59a22391009579ac7d92020
Gerald <gprmarquez@gmail.com>
2020-12-09 12:52:25 -0800

    added decrement button and functionality is working so far

--- src/screens/givr/donationForm.js
+++ src/screens/givr/donationForm.js
@@ -4,40 +4,39 @@ import { addDonation } from "../../services/firebase"
 import AuthContext from "../../../Context"
 
 // Data *delete once integrated w/ database*
-
-const sampleData =
-  [
-    {
-      "itemName": "Shirt",
-      "category": "Clothing",
-      "priority": "High"
-    },
-    {
-      "itemName": "Pants",
-      "category": "Clothing",
-      "priority": "Medium"
-    },
-    {
-      "itemName": "Food",
-      "category": "Canned Tomatos",
-      "priority": "Low"
-    },
-    {
-      "itemName": "Shirt",
-      "category": "Clothing",
-      "priority": "High"
-    },
-    {
-      "itemName": "Pants",
-      "category": "Clothing",
-      "priority": "Medium"
-    },
-    {
-      "itemName": "Food",
-      "category": "Canned Tomatos",
-      "priority": "Low"
-    }
-  ]
+const sampleData = 
+[
+  {
+    "itemName": "Shirt",
+    "category": "Clothing",
+    "priority": "High"
+  },
+  {
+    "itemName": "Pants",
+    "category": "Clothing",
+    "priority": "Medium"
+  },
+  {
+    "itemName": "Food",
+    "category": "Canned Tomatos",
+    "priority": "Low"
+  },
+  {
+    "itemName": "Shirt",
+    "category": "Clothing",
+    "priority": "High"
+  },
+  {
+    "itemName": "Pants",
+    "category": "Clothing",
+    "priority": "Medium"
+  },
+  {
+    "itemName": "Food",
+    "category": "Canned Tomatos",
+    "priority": "Low"
+  }
+]
 
 // Weird error about text can't be child of view
 
@@ -81,18 +80,18 @@ function DonationCard(props) {
 }
 
 // Donation Cards
-function DonationCardGallery(props) {
+function DonationCardGallery(props){
   const id = props.id;
   const charityID = props.charityID;
-  return (
+  return(
     <View style={styles.cardGallery}>
       {props.data.map((x, i) => {
-        return (
-          <DonationCard
-            data={x} cardID={i}
-            store={props.store}
-            key={i} id={id}
-            charityID={charityID}
+        return(
+          <DonationCard 
+              data = {x} cardID = {i} 
+              store = {props.store} 
+              key = {i} id = {id} 
+              charityID = {charityID}
           />
         )
       })}
@@ -104,49 +103,49 @@ function DonationCardGallery(props) {
 // Reach: add description feature
 
 export default function DonationForm(props) {
+    
+    const id = props.id;
+    const charityID = props.charityID;
 
-  const id = props.id;
-  const charityID = props.charityID;
+    // User ID
+    console.log("User & Charity ID", props.id, props.charityID);
 
-  // User ID
-  console.log("User & Charity ID", props.id, props.charityID);
+    // Replace data w/ firebase call | this is data that should be 
+    // sent back to firebase
+    const data = 
+      {
+      "Shirt": 0,
+      "Pants": 0,
+      "Food": 0
+      }
+    
+    // Gerald's add donation stuff
+    let donationList = [];
 
-  // Replace data w/ firebase call | this is data that should be
-  // sent back to firebase
-  const data =
-  {
-    "Shirt": 0,
-    "Pants": 0,
-    "Food": 0
-  }
+    const onDonationAdded = () => {
+        let data =
+        {
+            "name": name,
+            "quantity": quantity,
+            "priority": priority
+        }
 
-  // Gerald's add donation stuff
-  let donationList = [];
-
-  const onDonationAdded = () => {
-    let data =
-    {
-      "name": name,
-      "quantity": quantity,
-      "priority": priority
+        // Push data to firebase
+        /* Insert code here */
+        addDonation(data)
+        // Validation
+        console.log(data);
+        alert("Donation Added");
     }
-
-    // Push data to firebase
-    /* Insert code here */
-    addDonation(data)
-    // Validation
-    console.log(data);
-    alert("Donation Added");
-  }
 
   // Stored to prepare data that will be sent back to firebase
   return (
     <View style={styles.container}>
-      <DonationCardGallery data={sampleData} store={data} id={id} charityID={charityID} />
-      <TouchableOpacity
-        onPress={() => console.log(data)}
+      <DonationCardGallery data={sampleData} store = {data} id = {id} charityID = {charityID}/> 
+      <TouchableOpacity 
+        onPress={() => console.log(data)} 
         style={styles.submitButton}
-      >
+      > 
         <Text style={styles.buttonText}> Submit Donations </Text>
       </TouchableOpacity>
     </View>
@@ -184,6 +183,6 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 20,
-
+    
   }
 });

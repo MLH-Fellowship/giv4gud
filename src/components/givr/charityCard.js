20633e16962d9d27aff9b732f1407d8ef57bc7df
Gerald <gprmarquez@gmail.com>
2020-12-08 18:21:04 -0800

    getting orgs for charity card

--- src/components/givr/charityCard.js
+++ src/components/givr/charityCard.js
@@ -5,47 +5,85 @@ import { Card } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 
 // Get data (replace w/ call to Firebase later)
+import { db } from '../../services/firebase' // allows call database
+import 'firebase/firestore'
 import charity from "../../../data/charity"
 
 // Function to create Charity Cards
-export default function CharityCard(props){
-    
+export default function CharityCard(props) {
+
     // Get Navigator Object
     const navigation = useNavigation();
 
     // Get User ID (only used for navigation)
     const id = props.id;
     console.log("ID in Charity Card", props.id);
-    
+
     // Need to get collections from firebase to render cards
-    
-    return(
-    <Card>
-        <Card.Title> Charity Card </Card.Title>
-        <Card.Divider /> 
-        {
-        charity.map((u, i) => { // Replace charity w/ data from firebase Note: make sure i is replaced w/ charity document name
-            return (
-                <TouchableOpacity key = {i} onPress = {() => navigation.navigate("Open Charity", {id: id, charityID: i})}> 
-                    <View style = {styles.cardContainer}>
-                        <Text> {u.name} </Text> 
-                        <Text> Location: {u.location}</Text>
-                        <Text> Needs: {u.highNeeds}</Text>
-                        <Text> Key: {i} </Text>
-                    </View>
-                </TouchableOpacity>
-            );
+    var orgRef = db.collection('organizations');
+    const getOrgs = async () => {
+        let orgs = []
+        console.log('before await')
+        await orgRef.get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                orgs.push(doc.data())
+                console.log(doc.data())
+            })
         })
-        }
-    </Card>
+        console.log(orgs)
+        return orgs
+    }
+    console.log(getOrgs())
+    const organizations = getOrgs()
+    console.log(organizations)
+    return (
+        <>
+            <Card>
+                <Card.Title> Charity Card </Card.Title>
+                <Card.Divider />
+                {
+                    charity.map((u, i) => { // Replace charity w/ data from firebase Note: make sure i is replaced w/ charity document name
+                        return (
+                            <TouchableOpacity key={i} onPress={() => navigation.navigate("Open Charity", { id: id, charityID: i })}>
+                                <View style={styles.cardContainer}>
+                                    <Text> {u.name} </Text>
+                                    <Text> Location: {u.location}</Text>
+                                    <Text> Needs: {u.highNeeds}</Text>
+                                    <Text> Key: {i} </Text>
+                                </View>
+                            </TouchableOpacity>
+                        );
+                    })
+                }
+            </Card>
+
+            <Card>
+                <Card.Title> Charity Card </Card.Title>
+                <Card.Divider />
+                {
+                    charity.map((u, i) => { // Replace charity w/ data from firebase Note: make sure i is replaced w/ charity document name
+                        return (
+                            <TouchableOpacity key={i} onPress={() => navigation.navigate("Open Charity", { id: id, charityID: i })}>
+                                <View style={styles.cardContainer}>
+                                    <Text> {u.name} </Text>
+                                    <Text> Location: {u.location}</Text>
+                                    <Text> Needs: {u.highNeeds}</Text>
+                                    <Text> Key: {i} </Text>
+                                </View>
+                            </TouchableOpacity>
+                        );
+                    })
+                }
+            </Card>
+        </>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         padding: 0,
-        backgroundColor: 'lightblue'        
-      },
+        backgroundColor: 'lightblue'
+    },
     cardContainer: {
         margin: 10,
         borderColor: "red",
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -33,5 +33,4 @@ const addDonation = (data) => {
 export {
     db,
     addDonation,
-    getUserType,
 }
